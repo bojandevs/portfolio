22dea558b1c5315a1bae2e9c5b34a7d5100683e0
Bojan <152921900+bojandevs@users.noreply.github.com>
2025-06-12 17:06:09 +0200

    Add files via upload

--- app/components/ProfileSection.tsx
+++ app/components/ProfileSection.tsx
@@ -5,7 +5,7 @@ const ProfileSection = () => {
 		<div className="w-full py-8 md:py-0 bg-white dark:bg-gray-900 px-6 pt-16 sm:pt-0">
 			<div className="space-y-8 text-center">
 				<div className="relative w-28 md:w-40 h-28 md:h-40 mx-auto">
-					<div className="w-[160px] h-[165px] rounded-full overflow-hidden ring-2 ring-gray-200 dark:ring-gray-700 mx-auto">
+					<div className="w-[160px] h-[160px] rounded-full overflow-hidden ring-2 ring-gray-200 dark:ring-gray-700 mx-auto">
 						<Image
 							src="/portrait.png"
 							alt="Profile"
